97c3800b9d17abad2998d81c39446266140150b6
marcard <marcard.mx@outlook.com>
2019-11-18 13:04:03 -0400

    Databese Criado

--- node-api/server.js
+++ node-api/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 //iniciando o app
 const app = express();
 
+//iniciando DB
 
+mongoose.connect('mongodb://localhost:32769/nodeapi',
+{
+    useNewUrlParser: true,
+    useUnifiedTopology: true 
+});
 //Rotas
 app.get('/', function(req,res){
     res.send("Hello World");
